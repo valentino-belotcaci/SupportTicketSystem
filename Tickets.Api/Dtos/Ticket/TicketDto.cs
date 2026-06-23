0afcb3ad3fd3efead11aaef049d57b49f18e91be
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-06-23 22:49:35 +0200

    KAN-10:removed validation annotations in ticketDto, since its a response dto

--- Tickets.Api/Dtos/Ticket/TicketDto.cs
+++ Tickets.Api/Dtos/Ticket/TicketDto.cs
@@ -4,32 +4,14 @@ using Tickets.Api.Enums;
 namespace Tickets.Api.Dtos.Ticket
 {
     public class TicketDto
-    {
-        public Guid Id {    //property declaration ( field with getter and setter )
-            get;
-            set;
-        } = Guid.NewGuid();
-        
-        [Required]
-        [MinLength(3, ErrorMessage = "Title must be at least 3 characters")]
-        [MaxLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
-        public string Title { get; set; } = string.Empty;
-
-        [Required]
-        [MinLength(10, ErrorMessage = "Description must be at least 10 characters")]
-        public string Description { get; set; } = string.Empty;
-
-        public TicketStatus Status { get; set; }
-
-        [Required]
-        public TicketPriority Priority { get; set; }
-
-        [Required]
-        public TicketCategory Category { get; set; }
-
-        public string? AssignedTo { get; set; } // optional field
-
-        [Required]
-        public string CreatedBy { get; set; } = string.Empty;
-    }
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public TicketStatus Status { get; set; }
+    public TicketPriority Priority { get; set; }
+    public TicketCategory Category { get; set; }
+    public string? AssignedTo { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
+}
 }
