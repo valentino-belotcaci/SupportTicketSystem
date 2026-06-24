b52c222e50aa374ec47d06d744310d1883c2babc
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-06-24 12:59:57 +0200

    added interface and repository for notification

---
+++ Notifications.Api/Interfaces/INotificationInterface.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Tickets.Api.Dtos.Ticket;
+using Tickets.Api.Models;
+
+namespace Notifications.Api.Interfaces
+{
+    public interface INotificationInterface
+    {
+        
+    }
+}
---
+++ Notifications.Api/Repository/NotificationRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notifications.Api.Repository
+{
+    public class NotificationRepository
+    {
+        
+    }
+}
