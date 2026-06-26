69921ae51221e39f505cb3153f94e0528357d5ce
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-06-26 14:33:03 +0200

    KAN-94: wrote tests for getAllAsync function

--- Tickets.Api.Tests/Repository/TicketRepositoryTests.cs
+++ Tickets.Api.Tests/Repository/TicketRepositoryTests.cs
@@ -1,12 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Tickets.Api.Data;
+using Tickets.Api.Enums;
+using Tickets.Api.Interfaces;
+using Tickets.Api.Models;
+using Tickets.Api.Repository;
+using Tickets.Api.Dtos.Ticket;
 
 namespace Tickets.Api.Tests.Repository
 {
     public class TicketRepositoryTests
     {
-        
+        private ApplicationDBContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDBContext(options);
+        }
+
+
+        private TicketRepository CreateRepo(ApplicationDBContext context)
+        {
+            var publisher = new Mock<IRabbitMQPublisher>();
+            var logger = new Mock<ILogger<TicketRepository>>();
+
+            return new TicketRepository(context, publisher.Object, logger.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllTickets()
+        {
+            // ARRANGE
+            var context = GetDbContext();
+
+            context.Tickets.AddRange(
+                new Ticket { Title = "T1" },
+                new Ticket { Title = "T2" }
+            );
+
+            await context.SaveChangesAsync();
+
+            var repo = CreateRepo(context);
+
+            // ACT
+            var result = await repo.GetAllAsync();
+
+            // ASSERT
+            result.Should().HaveCount(2);
+        }
     }
 }
