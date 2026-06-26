07eb71290d3c9921f2684badf15a737960e35817
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-06-26 13:53:21 +0200

    KAN-81:wrote tests for getAll function

---
+++ Notifications.Api.Tests/Controllers/NotificationControllerTests.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Notifications.Api.Controllers;
+using Notifications.Api.Dtos;
+using Notifications.Api.Interfaces;
+using Notifications.Api.Models;
+
+namespace Notifications.Api.Tests.Controllers
+{
+    public class NotificationControllerTests
+    {
+        //mock replaces the real repository (no DB needed)
+        private readonly Mock<INotificationRepository> _mockRepo;
+        private readonly NotificationController _controller;
+
+
+        public NotificationControllerTests()
+        {
+            //create fake repository
+            _mockRepo = new Mock<INotificationRepository>();
+
+            //inject fake repo into controller
+            _controller = new NotificationController(_mockRepo.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOkWithAllNotifications()
+        {
+            // ARRANGE
+
+            var fakeNotifications = new List<Notification>
+            {
+                new Notification
+                {
+                    Id = Guid.NewGuid(),
+                    TicketId = Guid.NewGuid(),
+                    Message = "Ticket created",
+                    Type = "Created"
+                },
+                new Notification
+                {
+                    Id = Guid.NewGuid(),
+                    TicketId = Guid.NewGuid(),
+                    Message = "Status changed",
+                    Type = "StatusChanged"
+                }
+            };
+
+            _mockRepo
+                .Setup(repo => repo.GetAllAsync())
+                .ReturnsAsync(fakeNotifications);
+
+            // ACT
+            var result = await _controller.GetAll();
+
+            // ASSERT
+            var okResult = result.Should()
+                .BeOfType<OkObjectResult>()
+                .Subject;
+
+            var notifications = okResult.Value
+                .Should()
+                .BeAssignableTo<List<NotificationDto>>()
+                .Subject;
+
+            notifications.Should().HaveCount(2);
+
+            notifications[0].Message
+                .Should()
+                .Be("Ticket created");
+
+            notifications[1].Message
+                .Should()
+                .Be("Status changed");
+        }
+
+    }
+}
